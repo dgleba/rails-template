b947e53f13691de4ba8ce732fd5eef4393256275
David Gleba <dgleba@gmail.com>
2016-06-03 22:29:23 -0400

    dg1

--- template.rb
+++ template.rb
@@ -4,25 +4,25 @@ def apply_template!
   assert_minimum_rails_version
   add_template_repository_to_source_path
 
-  remove_file 'README.rdoc'
-  template 'README.md.tt', force: true
-  template 'DEPLOYMENT.md.tt', force: true
+  # remove_file 'README.rdoc'
+  # template 'README.md.tt', force: true
+  # template 'DEPLOYMENT.md.tt', force: true
 
-  template 'Gemfile.tt', force: true
+  # template 'Gemfile.tt', force: true
 
-  template 'example.env.tt', 'example.env'
-  template 'ruby-version.tt', '.ruby-version'
-  template 'erdconfig.tt', '.erdconfig'
-  copy_file 'rubocop.yml', '.rubocop.yml'
-  copy_file '.capistrano/metrics'
+  # template 'example.env.tt', 'example.env'
+  # template 'ruby-version.tt', '.ruby-version'
+  # template 'erdconfig.tt', '.erdconfig'
+  # copy_file 'rubocop.yml', '.rubocop.yml'
+  # copy_file '.capistrano/metrics'
   copy_file 'gitignore', '.gitignore', force: true
-  copy_file 'Procfile'
+  # copy_file 'Procfile'
 
-  apply 'app/template.rb'
-  apply 'config/template.rb'
-  apply 'bin/template.rb'
-  apply 'lib/template.rb'
-  apply 'vendor/template.rb'
+  # apply 'app/template.rb'
+  # apply 'config/template.rb'
+  # apply 'bin/template.rb'
+  # apply 'lib/template.rb'
+  # apply 'vendor/template.rb'
 
   run 'bundle install --quiet'
 
@@ -54,7 +54,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       '--quiet',
-      'https://github.com/manuelvanrijn/rails-template.git',
+      'https://github.com/dgleba/rails-template.git',
       tempdir
     ].map(&:shellescape).join(' ')
   else
