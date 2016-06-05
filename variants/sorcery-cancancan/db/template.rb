50e680935182459df85ddb39153d171891179c55
David Gleba <dgleba@gmail.com>
2016-06-05 09:56:58 -0400

    dg4

--- variants/sorcery-cancancan/db/template.rb
+++ variants/sorcery-cancancan/db/template.rb
@@ -1,7 +1,7 @@
 # ask seed input
 # email = ask_wizard_with_default('Please input the admin emailaddress', 'a@e')
 # pass = ask_wizard_with_default('Please input the admin password', 'a')
-email = a@e'
+email = 'a@e'
 pass = 'a'
 
 seed = "User.create! email: '#{email}', password: '#{pass}', password_confirmation: '#{pass}'"
