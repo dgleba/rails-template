1d23de82c9052a960b9e23a9b2f28be7cf3f50d0
David Gleba <dgleba@gmail.com>
2016-06-05 20:01:55 -0400

    dg35

--- variants/sorcery-cancancan/app/models/user.rb
+++ variants/sorcery-cancancan/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   #validates :email, presence: true, uniqueness: true, email: true
-  validates :email, presence: true, uniqueness: true, 
+  validates :email, presence: true, uniqueness: true 
   validates :password, length: { minimum: 1 }, if: :password_update?
   validates :password, confirmation: true
   validates :password_confirmation, presence: true, if: :password_update?
